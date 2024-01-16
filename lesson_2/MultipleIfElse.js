64581d9eaf7d24f128ae2f58504ddfb4cce06b7a
yenpt2911 <yenpt2911@gmail.com>
2024-01-16 23:15:36 +0700

    Adding lession 2

---
+++ lesson_2/MultipleIfElse.js
@@ -0,0 +1,11 @@
+// <18 | 18-55 | >55
+
+let clientAge = 17;
+
+if(clientAge < 18) {
+    console.log("Con nit ma doi uong bia, khong ban!")
+} else if(clientAge >= 18 && clientAge <= 55){
+    console.log("Unlimited!")
+} else {
+    console.log("2 chai!")
+}
---
+++ lesson_2/OperatorLearning.js
@@ -0,0 +1,13 @@
+let myNum1 = 1;
+let myNum2 = 2;
+
+// Rught operand and left operand
+let total = myNum1 + myNum2;
+console.log('Total: ', total);
+
+// Chia lay phan du
+console.log('5 chia 2, phan du la: ',5 % 2);
+
+console.log(5/2);
+
+// Rounding
